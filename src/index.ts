f3eca6d235ed071a224b9aec35d35e210f7a92f8
Muharrem Sönmez <msonmez@Muharrems-MacBook-Pro.local>
2020-09-09 15:57:56 +0200

    Changes server to apollo-server-express to use express app

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import 'reflect-metadata'
+import dotenv from 'dotenv'
+import express from 'express'
 import {createConnection} from 'typeorm'
-import {ApolloServer} from 'apollo-server'
+import {ApolloServer} from 'apollo-server-express'
 import {buildSchema} from 'type-graphql'
 
 import {TodoResolver} from './resolvers/TodoResolver'
 
+dotenv.config()
+
 async function bootstrap() {
+  const app = express()
   await createConnection()
   const schema = await buildSchema({resolvers: [TodoResolver]})
   const server = new ApolloServer({schema, playground: true})
+  server.applyMiddleware({app})
 
-  const {url} = await server.listen(4000)
-  console.log(`Server is running :)  - GraphQl playground available at: ${url}`)
+  app.listen(4000, () => {
+    console.log(`Server is running :)  - GraphQl playground available at: ${server.graphqlPath}`)
+  })
 }
 
 bootstrap()
